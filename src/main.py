84faa1c9db10d356146cfa32d5392c4e4fcb6989
Kostas <konstgyftodimos@gmail.com>
2023-09-25 19:48:44 +0300

    MPS GPU env setup

--- src/main.py
+++ src/main.py
@@ -1,2 +1,13 @@
-import sys
-print(sys.version)
+# Libraries
+import torch
+
+def mps_check():
+    """
+    Check MPU GPU availability
+    """
+    print(f"Pytorch version: {torch.__version__}")
+    print(f"Is MPS (Metal Performance Shader) built? {torch.backends.mps.is_built()}")
+    print(f"Is MPS (Metal Performance Shader) available? {torch.backends.mps.is_available()}")
+
+if __name__ == '__main__':
+    mps_check()
